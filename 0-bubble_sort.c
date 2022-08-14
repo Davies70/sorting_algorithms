f2cc9c56204330ffbd48e1694cff0aaf2a3e0e35
Ajayi <ajayidavies7@gmail.com>
2022-08-14 02:49:30 -0700

    comment

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,24 +7,24 @@
  * Return: nothing
  *
  */
-void bubble_sort(int *array, size_t n)
+void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	size_t j = 0;
 	int temp;
 
-	if (array == NULL || n < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < (n - 1); j++)
+		for (j = 0; j < (size - 1); j++)
 		{
 			if (array[j + 1] < array[j])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				print_array(array, n);
+				print_array(array, size);
 			}
 		}
 	}
