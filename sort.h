c667c369d3b2c04d6dcdfb3416652bf4674c2cf7
Ajayi <ajayidavies7@gmail.com>
2022-08-16 01:50:23 -0700

    comment

--- sort.h
+++ sort.h
@@ -18,6 +18,20 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
