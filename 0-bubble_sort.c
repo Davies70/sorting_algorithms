e2c9efb31a8a4398fd9d0b8ce023310fe254496e
Ajayi <ajayidavies7@gmail.com>
2022-08-16 02:33:39 -0700

    comment

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,6 +23,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	char isSorted;
 
+	if (size < 2)
+		return;
 	for (i = 1; i < size; i++)
 	{
 		isSorted = 1;
@@ -32,7 +34,7 @@ void bubble_sort(int *array, size_t size)
 			{
 				swap(&array[j], &array[j + 1]);
 				isSorted = 0;
-				print_array(array, size);
+				print_array((const *int)array, size);
 			}
 		}
 		/* if no two elements were swapped by inner loop, then break */
